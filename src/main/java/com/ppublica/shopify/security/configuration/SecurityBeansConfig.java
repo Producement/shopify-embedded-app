ae773527715d2f1267f479ac3f2502cf5ac8fe08
yerzhansarbassov <sardieri@gmail.com>
2021-03-18 20:45:29 +0600

    ClientRegistrationRepository bean

--- src/main/java/com/ppublica/shopify/security/configuration/SecurityBeansConfig.java
+++ src/main/java/com/ppublica/shopify/security/configuration/SecurityBeansConfig.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -189,5 +191,10 @@ public class SecurityBeansConfig {
 	@Bean
 	public ShopifyOAuth2 shopifyOAuth2(ShopifyPaths shopifyPaths, ClientRegistration shopifyClientRegistration) {
 		return new ShopifyOAuth2(shopifyPaths, shopifyClientRegistration);
+	}
+
+	@Bean
+	public ClientRegistrationRepository clientRegistrationRepository(ClientRegistration shopifyClientRegistration) {
+		return new InMemoryClientRegistrationRepository(shopifyClientRegistration);
 	}
 }
